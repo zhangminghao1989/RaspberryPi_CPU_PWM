c7cf5fed710beaa0c0b7fb96702a92f5043d0333
tankririri <tankririri@users.noreply.github.com>
2017-01-14 14:29:01 +0800

    Update cpu_fan.sh

--- cpu_fan.sh
+++ cpu_fan.sh
@@ -8,7 +8,9 @@ chmod 777 $RUN
 #设置风扇默认运行模式，0为关闭，1为全速，2为自动，运行过程中可以直接修改/var/run/cpu-fan.run文件来生效
 echo "2" > $RUN
 #设置开启风扇的最低温度
-set_temp_min=35000
+set_temp_min=3500
+#设置关闭风扇温度比最低温度小1度
+shutdown_temp=`expr $set_temp_min - 1000`
 #设置风扇全速运行的温度
 set_temp_max=70000
 
@@ -37,11 +39,10 @@ fi
 
 
 #小于设置温度关闭风扇
-if [ $tmp -le $set_temp_min ] ;then
+if [ $tmp -le $shutdown_temp ] ;then
 pwm=0
 fan=0
 else
-
 #第一次超过设置温度全速开启风扇，防止风扇不能启动
 if [ $tmp -gt $set_temp_min ] && [ $fan -eq 0 ] ;then
 gpio pwm 1 1023
