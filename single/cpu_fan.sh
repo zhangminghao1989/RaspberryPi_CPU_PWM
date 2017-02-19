4854a013d3e1a6ac06a4d2ac1bf4bdc913da6cac
Zhang Minghao <zhangminghao@vip.qq.com>
2017-02-19 23:40:19 +0800

    更新单文件运行版本

--- single/cpu_fan.sh
+++ single/cpu_fan.sh
@@ -2,6 +2,7 @@
 
 #sleep 5
 #设置运行状态文件
+LOG=/var/log/cpu-fan/cpu-fan.log
 RUN=/var/run/cpu-fan.run
 touch $RUN
 chmod 777 $RUN
@@ -27,7 +28,7 @@ while true
   do
   #获取cpu温度
   tmp=`cat /sys/class/thermal/thermal_zone0/temp`
-  status=`cat $RUN`
+  MODE=`cat $RUN`
   
   #计算pwm值，从变量set_temp_min设置的温度开始开启风扇，最低转速50%
   pwm=$((($tmp-$set_temp_min)*512/($set_temp_max-$set_temp_min)+511))
@@ -40,39 +41,48 @@ while true
   pwm=1023
   fi
   
-  
-  #小于设置温度关闭风扇
-  if [ $tmp -le $shutdown_temp ] ;then
-  pwm=0
-  fan=0
-  else
-  
   #第一次超过设置温度全速开启风扇，防止风扇不能启动
-  if [ $tmp -gt $set_temp_min ] && [ $fan -eq 0 ] ;then
+  if [ $tmp -gt $set_temp_min ] && [ $fan -eq 0 ]  && [ $MODE -eq 2 ] ;then
   gpio pwm 1 1023
   fan=1
+  echo "`date` temp=$tmp pwm=1023 MODE=$MODE CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`% 第一次超过设置温度全速开启风扇" >> $LOG
+  sleep 1
+  fi
+  
+
+  #小于设置温度关闭风扇
+if [ $fan -eq 0 ] ;then
+  pwm=0
+ fi
+ if [ $tmp -le $shutdown_temp ] && [ $MODE -eq 2 ] ;then
+  pwm=0
+  fan=0
+  gpio mode 1 pwm
+  gpio pwm 1 $pwm
   sleep 5
-  fi
-  fi
+  echo "`date` temp=$tmp pwm=$pwm MODE=$MODE CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`% 小于设置温度关闭风扇 " >> $LOG
+else
   
-  
-  #检查run文件，为0时关闭风扇
-  if [ $status -eq 0 ] ;then
+  #检查MODE，为0时关闭风扇
+  if [ $MODE -eq 0 ] ;then
   pwm=0
   fan=0
   else
   
   #检查run文件，为1时持续开启风扇最高转速
-  if [ $status -eq 1 ] ;then
+  if [ $MODE -eq 1 ] ;then
   pwm=1023
+  fan=1
   fi
   fi
-  
+
+  gpio mode 1 pwm
   gpio pwm 1 $pwm
   
+  #输出日志
+  echo "`date` temp=$tmp  pwm=$pwm MODE=$MODE CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`%" >> /var/log/cpu-fan/cpu-fan.log
+
   #每5秒钟检查一次温度
   sleep 5
-  
-  #输出日志
-  echo "`date` temp=$tmp  pwm=$pwm status=$status CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`%" >> /var/log/cpu-fan/cpu-fan.log
+fi
 done
