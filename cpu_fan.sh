56fc3f8fe9dec76f3b7f27ffb287c800dd5c3577
Zhang Minghao <zhangminghao@vip.qq.com>
2017-02-19 16:26:20 +0800

    更改运行方式 1.全面修改脚本，将程序注册成系统服务 2.将配置文件分离出来，可直接修改配置文件调整温度设置

--- cpu_fan.sh
+++ cpu_fan.sh
@@ -2,17 +2,8 @@
 
 #sleep 5
 #设置运行状态文件
-RUN=/var/run/cpu-fan.run
-touch $RUN
-chmod 777 $RUN
-#设置风扇默认运行模式，0为关闭，1为全速，2为自动，运行过程中可以直接修改/var/run/cpu-fan.run文件来生效
-echo "2" > $RUN
-#设置开启风扇的最低温度
-set_temp_min=35000
-#设置关闭风扇温度比最低温度小1度
-shutdown_temp=`expr $set_temp_min - 1000`
-#设置风扇全速运行的温度
-set_temp_max=70000
+CONF=$1
+LOG=$2
 
 #开机风扇全速运行
 #默认的pwm值范围是0~1023
@@ -27,7 +18,28 @@ while true
   do
   #获取cpu温度
   tmp=`cat /sys/class/thermal/thermal_zone0/temp`
-  status=`cat $RUN`
+
+  #读取配置
+  while read line; do
+	name=`echo $line | awk -F '=' '{print $1}'`
+	value=`echo $line | awk -F '=' '{print $2}'`
+	case $name in
+	"MODE")
+	MODE=$value
+	;;
+	"set_temp_min")
+	set_temp_min=$value
+	;;
+	"shutdown_temp")
+	shutdown_temp=$value
+	;;
+	"set_temp_max")
+	set_temp_max=$value
+	;;
+	*)
+	;;
+	esac
+  done < /home/pi/.cpu-fan.conf
   
   #计算pwm值，从变量set_temp_min设置的温度开始开启风扇，最低转速50%
   pwm=$((($tmp-$set_temp_min)*512/($set_temp_max-$set_temp_min)+511))
@@ -39,40 +51,46 @@ while true
   if [ $pwm -gt 1023 ] ;then
   pwm=1023
   fi
-  
-  
-  #小于设置温度关闭风扇
-  if [ $tmp -le $shutdown_temp ] ;then
-  pwm=0
-  fan=0
-  else
-  
+    
   #第一次超过设置温度全速开启风扇，防止风扇不能启动
   if [ $tmp -gt $set_temp_min ] && [ $fan -eq 0 ] ;then
   gpio pwm 1 1023
   fan=1
+  echo "`date` temp=$tmp pwm=$pwm MODE=$MODE CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`% 第一次超过设置温度全速开启风扇" >> $LOG
+  sleep 1
+  fi
+ 
+  #小于设置温度关闭风扇
+  if [ $fan -eq 0 ] ;then
+  pwm=0
+  fi
+  if [ $tmp -le $shutdown_temp ] && [ $MODE -eq 2 ] ;then
+  pwm=0
+  fan=0
+  gpio pwm 1 $pwm
   sleep 5
-  fi
-  fi
-  
-  
-  #检查run文件，为0时关闭风扇
-  if [ $status -eq 0 ] ;then
+    echo "`date` temp=$tmp pwm=$pwm MODE=$MODE CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`% 小于设置温度关闭风扇 " >> $LOG
+  else
+
+  #检查MODE，为0时关闭风扇
+  if [ $MODE -eq 0 ] ;then
   pwm=0
   fan=0
   else
   
-  #检查run文件，为1时持续开启风扇最高转速
-  if [ $status -eq 1 ] ;then
+  #检查MODE，为1时持续开启风扇最高转速
+  if [ $MODE -eq 1 ] ;then
   pwm=1023
   fi
   fi
   
   gpio pwm 1 $pwm
-  
+    
+  #输出日志
+  echo "`date` temp=$tmp pwm=$pwm MODE=$MODE CPU idle=`top -n 1|grep Cpu|awk '{print $8}'`%" >> $LOG
+
   #每5秒钟检查一次温度
   sleep 5
-  
-  #输出日志
-  echo "`date` temp=$tmp  pwm=$pwm status=$status CPU idle:`top -n 1|grep Cpu|awk '{print $8}'`%" >> /var/log/cpu-fan/cpu-fan.log
+
+  fi
 done
